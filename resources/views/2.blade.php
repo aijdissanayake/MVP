ce26533553df43a8088cc2758fecb12644e75d28
Achala Dissanayake <aijdissanayake@gmail.com>
2017-03-02 00:45:44 +0530

    Routes and cdn links added

--- resources/views/2.blade.php
+++ resources/views/2.blade.php
@@ -10,6 +10,11 @@
 
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<!-- Compiled and minified CSS -->
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+
+  	<!-- Compiled and minified JavaScript -->
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
 
 	<style>
 		.top-row{padding-top: 3rem;}
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('1');
+});
+
+Route::get('/next', function () {
+    return view('2');
 });
