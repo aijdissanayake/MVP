b424cf5069e802e839c5f464e4a444cd74080873
Achala Dissanayake <aijdissanayake@gmail.com>
2017-03-02 10:39:00 +0530

    JQuery intialization and Material Select

--- resources/views/1.blade.php
+++ resources/views/1.blade.php
@@ -12,8 +12,9 @@
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 	<!-- Compiled and minified CSS -->
   	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
-
-  	<!-- Compiled and minified JavaScript -->
+  	<!--JQuery-->
+  	<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>  
+	<!-- Compiled and minified JavaScript -->
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
 
 	<style>
@@ -61,6 +62,13 @@
 					margin-top:-32767px;/
 				}
 			</style>
+
+			<script type="text/javascript">
+				$(document).ready(function() {
+					$('select').material_select();
+				});
+			</script>
+
 		</head>
 		<body class="red">
 
